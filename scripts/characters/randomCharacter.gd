dc73466099045c18dcfb4c4d383358f22e6a1664
Anhony <anthony.violet@outlook.be>
2024-01-27 18:09:35 +0100

    add script for randomCharacter
    multi directional inputs

--- scripts/characters/randomCharacter.gd
+++ scripts/characters/randomCharacter.gd
@@ -1,18 +1,10 @@
-extends Node3D
+extends Sprite3D
 
-var speed = 5;
-
+var speed = 5
 
 func _process(delta):
-	var direction = Vector3()  # Initialisez un vecteur pour le mouvement
-
-	if Input.is_action_pressed("ui_up"):
-		direction.z -= 1  # Déplacement vers l'avant
-	if Input.is_action_pressed("ui_down"):
-		direction.z += 1  # Déplacement vers l'arrière
-
-	# Normalisez le vecteur pour éviter le mouvement plus rapide en diagonale
-	direction = direction.normalized()
-
-	# Appliquez le mouvement à la position du nœud
-	translate(direction * speed * delta)
+	var input_z = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	var input_h = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	
+	translate(Vector3(input_h, 0, input_z) * delta * speed)
