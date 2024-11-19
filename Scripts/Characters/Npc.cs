093b0d615771afb79d1eda9ac4b7dd3755056ba9
MrToine <anthony.violet@proton.me>
2024-11-19 16:16:19 +0100

    update npc and enemy classes

--- Scripts/Characters/Npc.cs
+++ Scripts/Characters/Npc.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Npc : Node
+public partial class Npc : Character
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
